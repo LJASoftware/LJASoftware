77db55e6823badcd1f596a02966a67cf69353518
lyzzandro <lyzzandro91@gmail.com>
2022-02-16 15:59:58 -0300

    Estilizando a sessão de contato

--- ljasoftware/components/Contato/Contato.component.jsx
+++ ljasoftware/components/Contato/Contato.component.jsx
@@ -1,5 +1,18 @@
 import styles from './Contato.style.module.css'
 
 export function Contato() {
-  return <form>CONTATO</form>
+  return <div className={styles.contato}>
+
+    <h2>Contato</h2>
+
+      <form>
+        <label>Nome</label>
+        <input type="text" placeholder='Digite seu nome...'></input>
+        <label>E-mail</label>
+        <input type="email" placeholder='Digite seu e-mail...'></input>
+        <label>Mensagem</label>
+        <textarea placeholder='Digite sua mensagem...'></textarea>
+      </form>
+
+    </div>
 }
