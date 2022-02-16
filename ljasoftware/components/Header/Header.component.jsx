bcd1258ba8a57ca717dc9af41cd1b6ebb748adf3
lyzzandro <lyzzandro91@gmail.com>
2022-02-16 14:51:44 -0300

    Estilizando Header e componente Slogan

--- ljasoftware/components/Header/Header.component.jsx
+++ ljasoftware/components/Header/Header.component.jsx
@@ -1,5 +1,21 @@
 import styles from './Header.style.module.css'
+import Image from 'next/image'
 
 export function Header() {
-  return <header className={styles.azul}>AKI VAI O HTML DO HEADER</header>
+  return <header className={styles.header}>
+
+  <Image
+      width="50"
+      height="50"
+      src="/logo.png"
+      alt="Picture of the author"
+    />
+
+    <ul>
+      <li><a href='#'>SOBRE</a></li>
+      <li><a href='#'>PORTIFÓLIO</a></li>
+      <li><a href='#'>CONTATO</a></li>
+    </ul>
+
+  </header>
 }
--- ljasoftware/components/Slogan/Slogan.component.jsx
+++ ljasoftware/components/Slogan/Slogan.component.jsx
@@ -1,5 +1,11 @@
 import styles from './Slogan.style.module.css'
+import Image from 'next/image'
+
 
 export function Slogan() {
-  return <div>SLOGAN</div>
+  return <div className={styles.slogan}>
+
+    <h1>LJA SEU PROJETO NUNCA FOI TÃO FÁCIL DE RODAR!</h1>
+
+  </div>
 }
